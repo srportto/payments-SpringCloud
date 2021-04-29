2881730b37c4b08b604f36c06a185127e133a34c
srportto <ceeh1100@gmail.com>
2021-04-29 17:51:05 -0300

    Implementes feignClient

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -3,7 +3,6 @@ package com.devsuperior.hrpayroll.entities;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
 @Data                                               //add toString, equalsAndHascode, getters , setters via lombok
@@ -14,8 +13,6 @@ public class Worker implements Serializable {
     public static final long serialVerionUID = 1L;
 
     @EqualsAndHashCode.Include  //informacao ao lombok que esse atributo deve ser considerado pra gerar equals and hashcode
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private  Double dailyIncome;        //quanto que o cara ganha por dia
